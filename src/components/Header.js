b186c88b1072e3459fe2e81fe1db9519abbe8033
Brandon Corey <bcorey85@gmail.com>
2020-01-28 02:57:38 -0600

    fix case sensitivity issue

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../sass/Header.module.scss';
+import logo from '../images/potwin-construction-logo-main.svg';
+import MainNav from './MainNav';
+
+const Header = () => {
+	return (
+		<header className={styles.header}>
+			<img
+				src={logo}
+				alt='Potwin Construction logo'
+				className={styles.logo}
+			/>
+			<MainNav />
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import '../sass/Layout.scss';
+
+const Layout = ({ children }) => {
+	return (
+		<div className='layout'>
+			<Header />
+			<Main>{children}</Main>
+			<Footer />
+		</div>
+	);
+};
+
+export default Layout;

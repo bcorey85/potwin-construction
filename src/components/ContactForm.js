148fbeb27fb7da87744adfd5ba058f0309188472
Brandon Corey <bcorey85@gmail.com>
2020-01-28 18:19:14 -0600

    fix email placeholder text

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,7 +9,7 @@ const ContactForm = () => {
 			<label htmlFor='name'>Name</label>
 			<input type='text' name='name' id='name' placeholder='Name' />
 			<label htmlFor='email'>Email</label>
-			<input type='email' name='email' id='email' placeholder='Name' />
+			<input type='email' name='email' id='email' placeholder='Email' />
 			<label htmlFor='comments'>Comments</label>
 			<textarea name='comments' id='comments' placeholder='Comments' />
 			<Button element='button' type='primary'>
